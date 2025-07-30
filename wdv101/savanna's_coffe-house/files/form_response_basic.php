95acff85f7b109d7fcd67fca2613e1b5442d3d8d
jalvarez0497 <jean.alvarez0497@gmail.com>
2025-07-29 22:12:37 -0500

    Completed the website proposal. Changes to all the pages.

--- wdv101/savanna's_coffe-house/files/form_response_basic.php
+++ wdv101/savanna's_coffe-house/files/form_response_basic.php
@@ -19,23 +19,11 @@
 
 <body>
 <div id="container">
-<h1>WDV101 Intro HTML and CSS</h1>
-<h2> Form Response Porcessor</h2>
-
-<p>This process will process the 'name = value' pairs for all the elements of your contact form. It will format and respond by sending an html page containing a table with the information you entered in the form.
-</p>
-
-<p>Instructions:</p>
-<ol>
-  <li>To call this page use <strong>form_response_basic.php</strong> in the action attribute of your form.</li>
-  <li>Make sure you choose method=&quot;post&quot;.</li>
-  <li>Ensure a link to this form from your homework page</li>
-  <li>Upload the updates homework page, the form.html page AND the php processor file to your host server. NOTE: the html form page AND the processor PHP page must be in the same location!</li>
-  </ol>
+<h1>Thanks for your submission</h1>
+<p>We will reach back to you in 1 to 2 business days.</p>
 
 </div><!--close div container-->
 
-<p>RESULT WILL DISPLAY BELOW THIS LINE</p>
 <hr>
 <p>&nbsp;</p>
 
